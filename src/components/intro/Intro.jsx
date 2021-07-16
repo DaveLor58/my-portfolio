d0650440123afe41d70513a6eb964d67e7672d59
unknown <Dave>
2021-07-16 09:48:09 -0700

    adding in components

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,39 @@
+import "./intro.scss"
+import {ExpandMore} from "@material-ui/icons";
+import { useEffect, useRef } from "react";
+import {init} from "ityped";
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() =>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["A College Student!", "An Aspiring Software Engineer!","A Programming Enthusiast!"],
+        });
+    },[]);
+
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/author_pic.png" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello There, I'm</h2>
+                    <h1>Dave Lorenz Castillo,</h1>
+                    <h3>
+                        <span ref={textRef}></span>
+                    </h3>
+                </div>
+                <a href="#portfolio">
+                    <ExpandMore className="icon"/>
+                </a>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,21 @@
+import "./portfolio.scss"
+
+export default function portfolio() {
+    return (
+        <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li>React Apps</li>
+                <li>Python Apps</li>
+                <li>C++/C# Apps</li>
+                <li>School Projects</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt=""/>
+
+                </div>
+            </div>
+        </div>
+    )
+}
